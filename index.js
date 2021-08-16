9383011d9a780c00b3ffd7e8a8666697953d40bc
Conner Gleason <connereg@optonline.net>
2021-08-16 10:32:19 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('p').textContent = ('JavaScript is so cool. It lets me add text to my page programmatically.', 'This is really cool!');
+
+  });
+
+console.log('maybe')
